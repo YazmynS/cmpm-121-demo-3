f0ab69b58089967f1a580e6a2d506d5aee9a2cbd
YazmynS <122718475+YazmynS@users.noreply.github.com>
2024-11-01 17:51:28 -0700

    Dialog Button Added (Complete)

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// todo
+//Task 1: Add Message Button
+const button = document.createElement("button");
+button.textContent = "Say Hi!";
+
+//Add Message Button Functionality
+button.addEventListener("click", () => {
+  alert("You said Hi!");
+});
+
+// Put  Message Button on Page
+document.body.appendChild(button);
