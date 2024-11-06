7a0f31bef8dbeba0337c97b187ce5e3a03ba0a20
Yazmyn Sims <yzsims@ucsc.edu>
2024-11-06 12:03:31 -0800

    Map Displays with Player and Cache Markers

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,66 @@
-//Task 1: Add Message Button
-const button = document.createElement("button");
-button.textContent = "Say Hi!";
+import leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+import "./leafletWorkaround.ts";
+import luck from "./luck.ts";
 
-//Add Message Button Functionality
-button.addEventListener("click", () => {
-  alert("You said Hi!");
+//classroom location
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+//gamplay parameters
+//const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create Map
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: 19,
+  minZoom: 19,
+  maxZoom: 19,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-// Put  Message Button on Page
-document.body.appendChild(button);
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add Player Marker
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+// Create and Display Caches at a location
+function spawnCache(i: number, j: number) {
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  //Rectangles represent cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+  rect.bindPopup(`Cache located at cell (${i}, ${j})`);
+}
+
+// Create Potential Cache Locations
+function generateCacheLocations() {
+  for (let i = -NEIGHBORHOOD_SIZE; i <= NEIGHBORHOOD_SIZE; i++) {
+    for (let j = -NEIGHBORHOOD_SIZE; j <= NEIGHBORHOOD_SIZE; j++) {
+      // Determine if a cache should be spawned at this grid cell
+      if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+        spawnCache(i, j);
+      }
+    }
+  }
+}
+
+// Generate caches around the player's initial location
+generateCacheLocations();
